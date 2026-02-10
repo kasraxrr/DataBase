05d73aec3b22c26faa0a92f8b0e9c30176bbcacb
kasraxrr <kasrafar1383@gmail.com>
2026-02-10 23:18:53 +0100

    jhvjgj

--- Handin1.sql
+++ Handin1.sql
@@ -1,4 +1,14 @@
 SET SCHEMA 'goodreads_v2';
 
+--TaskA
 SELECT id,title,year_published as pub_year
 FROM book;
+
+SELECT DISTINCT year_published
+FROM book;
+/*DISTINCT deletes the duplicated rows*/
+
+ SELECT year_published*2
+ FROM book;
+/*calculates the published year *2 */
+
