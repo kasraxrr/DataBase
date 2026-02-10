b45c2877527b7256c13f4d51ed9efd539f4d3c2e
kasraxrr <kasrafar1383@gmail.com>
2026-02-10 16:04:49 +0100

    jhvjgj

--- goodreadsMAIN.sql
+++ goodreadsMAIN.sql
@@ -1,17 +1,41 @@
 SET SCHEMA 'goodreads_v2';
 
---task1:
+--task2:
 SELECT first_name,last_name,id
 FROM author
 WHERE id=23;
 
---task2:
+--task3:
 SELECT title
 FROM book
 where id=24358527;
 
---task3:
+--task4:
+SELECT COUNT(*)
+FROM profile;
 
+--task5:
+SELECT COUNT(*)
+FROM profile
+WHERE first_name='Jaxx';
+
+--task6
+SELECT first_name,COUNT(first_name)
+from author
+GROUP BY first_name
+ORDER BY first_name;
+
+--task7
+SELECT title,page_count
+FROM book
+WHERE page_count IS NOT NULL
+ORDER BY page_count DESC ;
+
+--task8
+SELECT title,page_count
+FROM book
+WHERE page_count IS NOT NULL
+ORDER BY page_count DESC ;
 
 
 
